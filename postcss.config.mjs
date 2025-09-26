88fe0a54df6fe5b76308a79d81b52dc6d9e33cf6
raintype13 <a78057477@gmail.com>
2025-09-27 00:04:57 +0500

    fix: исправление ошибки сборки Tailwind CSS

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,5 +1,8 @@
-const config = {
-  plugins: ["@tailwindcss/postcss"],
-};
-
-export default config;
+// postcss.config.mjs
+export default {
+  plugins: {
+    // ЗАМЕНИТЕ СТАРУЮ СТРОКУ 'tailwindcss' НА @tailwindcss/postcss
+    '@tailwindcss/postcss': {}, 
+    'autoprefixer': {},
+  },
+}
